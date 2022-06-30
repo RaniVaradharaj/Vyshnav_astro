f1efa95eef785077120c6a3668c660c4b83fb14c
RaniVaradharaj <ranidvhsr31@gmail.com>
2022-06-30 17:34:26 +0530

    booting

--- vyshnav_web/src/App.js
+++ vyshnav_web/src/App.js
@@ -1,19 +1,37 @@
 // import logo from './logo.svg';
 import "./App.css";
+import About from "./components/about";
 
-// import galaxy from "C:\Users\vishn\OneDrive\Documents\FULLSTACK DEVELOPMENT - RANI\Vyshnav_astro\vyshnav_web\src\images\galaxy.jpg";
+// import moon from ".images/moon.jpg";
+var galaxy = require("../src/");
 
 function App() {
   return (
     <div className="App">
       <h1>VYSHNAV-ASTRO-NUMEROLGICAL CARE</h1>
       <div className="gallery">
-        {/* <img src={img1} alt="images" /> */}
-        <img src="img2.jpeg" alt="images" />
-        <img src="img3.jpeg" alt="images" />
-        <img src="img4.jpeg" alt="images" />
-        <img src="img5.jpeg" alt="images" />
+        <img
+          src="https://media.istockphoto.com/photos/astronaut-in-outer-space-spaceman-with-starry-and-galactic-background-picture-id1353874144?s=612x612"
+          alt="images"
+        />
+        <img
+          src="https://media.istockphoto.com/photos/astronaut-in-outer-space-spaceman-with-starry-and-galactic-background-picture-id1353874144?s=612x612"
+          alt="images"
+        />
+        <img
+          src="https://media.istockphoto.com/photos/astronaut-in-outer-space-spaceman-with-starry-and-galactic-background-picture-id1353874144?s=612x612"
+          alt="images"
+        />
+        <img
+          src="https://media.istockphoto.com/photos/astronaut-in-outer-space-spaceman-with-starry-and-galactic-background-picture-id1353874144?s=612x612"
+          alt="images"
+        />
+        <img
+          src="https://media.istockphoto.com/photos/astronaut-in-outer-space-spaceman-with-starry-and-galactic-background-picture-id1353874144?s=612x612"
+          alt="images"
+        />
       </div>
+      <About />
     </div>
   );
 }
---
+++ vyshnav_web/src/components/about.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="container">
+      Vyshnav Astronumerological Care was established in 2004 at Hosur,
+      Tamilnadu. You are heartily invited to the world of Vyshnav Astrology, the
+      celestial arena of stars and planets. The position of the stars and
+      planets impact your well being on physical, financial, mental and other
+      aspects. So, the proficient and one of the best astrologers in Tamilnadu,
+      Mrs.N.S. CHITRA, B.Sc.,B.Ed.,(Maths), M.A., M.Phil.,(Astrology) can help
+      you out to know about the movement of your planets. The prediction and
+      solutions provided by this expert will assist you to know your upcoming
+      good and bad times, and her advices will help you to find bright way by
+      discarding the clouds of darkness. You can know the weekly forecast,
+      monthly forecast through magazines but it is not for a specific person,
+      So, You have to know the forecast for some years, specifically based on
+      your Birth date, time &amp; place. This specific forecast for you will
+      help to know some critical situation and use a specific period of time to
+      do some good things in the life.
+    </div>
+  );
+}
+
+export default About;
